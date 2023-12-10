7017c26612bbae32affd1eb313ff806533a38105
Michał Zelek <101725721+MikeZ7@users.noreply.github.com>
2023-12-10 17:31:04 +0100

    Update Data.h

--- inc/Data.h
+++ inc/Data.h
@@ -3,16 +3,14 @@
 #include <cstdlib>
 #include <string>
 #include <fstream>
+
 using std::string;
 
 class Data
 {
 public:
-
 	static std::map <int, string> dataBase;
 
 	static void fillDataBase(int);
-
-
 };
 
