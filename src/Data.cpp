99e5e768a545298bd016e8227bd5982beaae233a
Michał Zelek <101725721+MikeZ7@users.noreply.github.com>
2023-12-10 17:33:13 +0100

    Update Data.cpp

--- src/Data.cpp
+++ src/Data.cpp
@@ -3,35 +3,28 @@
 
 std::map<int, string> Data::dataBase;
 
+
 void Data::fillDataBase(int categoryChoice) {
 	dataBase = {};
 	std::string line;
 	if (categoryChoice == 1) {
-		std::fstream file("flowers.txt");
-		
+		std::fstream file("flowers.txt");		
 		if (file.is_open()) {
 			while (file.eof() == 0) {
 				getline(file, line);
 				dataBase.insert({int(line[0])-'0',line.substr(2,line.length() - 2)});
-			}
-			
+			}	
 			file.close();
 		}
 	}
 	else if (categoryChoice == 2) {
 		std::fstream file("motorization.txt");
-
 		if (file.is_open()) {
 			while (file.eof() == 0) {
 				getline(file, line);
 				dataBase.insert({ int(line[0]) - '0',line.substr(2,line.length() - 2) });
 			}
-
 			file.close();
 		}
 	}
-
-
-
-
 }
